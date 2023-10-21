0c175a866d0fce9881a7910ed792c8a3d27a66ba
Parth Srivastava <134224360+luxorishere@users.noreply.github.com>
2023-10-21 11:13:35 +0530

    Create Longest_string_in_a_sentence.java

---
+++ Longest_string_in_a_sentence.java
@@ -0,0 +1,12 @@
+ static String longest_string(String string){
+        String[] str = string.split(" ");
+        int max = Integer.MIN_VALUE;
+        String string1 = "";
+        for (int i = 0; i < str.length; i++) {
+            if (str[i].length() > max){
+                max = str[i].length();
+                string1 = str[i];
+            }
+        }
+        return string1;
+    }
